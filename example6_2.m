f339cebb715d5386e63ead3b0f4effe6bb1d4a90
DavidJamesBen <56527030+DavidJamesBen@users.noreply.github.com>
2024-12-05 19:28:59 +0800

    Add files via upload

---
+++ example6_2.m
@@ -0,0 +1,10 @@
+% example6_2.m
+
+x=-4:.1:4;
+y=tansig(x);			% Tag-Sigmoidº¯Êı
+plot(x,y,'^-r')
+title('Tan-sig º¯Êı')
+xlabel('x')
+ylabel('y')
+grid on
+% web -broswer http://www.ilovematlab.cn/forum-222-1.html
